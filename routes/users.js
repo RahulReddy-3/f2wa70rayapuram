beb397476b4023ba41513d11b391e64b5573ebca
RahulReddy-3 <iamrahulreddy.3@gmail.com>
2022-10-26 17:59:08 -0500

    Updated users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var times = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  times += 1
+  res.send('User accesses are: ' + times);
 });
 
 module.exports = router;
